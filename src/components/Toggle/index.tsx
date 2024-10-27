7812e1d3c9c30d7a5e2d95dc111522cebc10be46
Todd Moy <todd.moy@gmail.com>
2024-10-26 22:21:41 -0600

    Refine Toggle

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,32 +1,73 @@
-import { useState } from 'react'
+import { useState, useEffect, CSSProperties } from 'react'
 
 interface ToggleProps {
-  isOn: boolean
-  onClick: () => void
+  isOn?: boolean
+  onClick?: () => void
+  onChange?: () => void
+  className?: string
+  style?: CSSProperties
+  disabled?: boolean
 }
 
-const Toggle: React.FC<ToggleProps> = ({ isOn = true, onClick }) => {
+const Toggle: React.FC<ToggleProps> = ({
+  isOn = true,
+  onClick,
+  onChange,
+  className,
+  style,
+  disabled = false,
+}) => {
   const [on, setOn] = useState(isOn)
 
   const handleClick = () => {
-    toggleState()
-    onClick()
-  }
+    // Optimistically update state.
+    toggleOnOff()
 
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === ' ') {
-      toggleState()
+    if (onClick) {
+      onClick()
     }
   }
 
-  const toggleState = () => {
-    setOn(!on)
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === ' ' || e.key === 'Enter') {
+      toggleOnOff()
+    }
   }
+
+  const toggleOnOff = () => {
+    if (!disabled) {
+      setOn(!on)
+    }
+  }
+
+  useEffect(() => {
+    if (onChange) {
+      onChange()
+    }
+  }, [on])
+
+  const cn = {
+    base: [
+      'bg-zinc-950',
+      'rounded-full',
+      'flex',
+      'items-center',
+      'p-[2px]',
+      'w-9',
+      'cursor-pointer',
+      'focus-visible:outline-offset-2',
+      'outline-purple-400',
+    ],
+    disabled: ['opacity-50 cursor-default'],
+  }
+
+  const classes =
+    [...cn.base, disabled ? cn.disabled : ''].join(' ') + ' ' + className
 
   return (
     <div
-      className="bg-zinc-950 rounded-full flex items-center p-[2px] w-9 cursor-pointer"
-      style={{ justifyContent: on ? 'flex-end' : 'flex-start' }}
+      className={classes}
+      style={{ justifyContent: on ? 'flex-end' : 'flex-start', ...style }}
       onClick={handleClick}
       onKeyDown={handleKeyPress}
       tabIndex={0}
