afbf6e60bab509268b01edd9ab3fdc572e1d61c5
Todd Moy <todd.moy@gmail.com>
2024-10-26 22:21:31 -0600

    Refine Tag

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,8 +1,13 @@
+import { CSSProperties } from 'react'
 import { FiX } from 'react-icons/fi'
 
 interface TagProps {
   label?: string
   icon?: ReactNode
+  size?: 'small' | 'medium'
+  color?: 'zinc' | 'red' | 'blue' | 'purple'
+  className?: string
+  style?: CSSProperties
   isRemovable?: boolean
   onClick?: () => void
   onRemove?: () => void
@@ -11,21 +16,43 @@ interface TagProps {
 const Tag: React.FC<TagProps> = ({
   label = 'Label',
   icon,
+  size = 'small',
+  color = 'zinc',
+  className,
+  style,
   isRemovable = false,
   onClick,
   onRemove,
-}) => (
-  <div
-    className="bg-zinc-950 text-white fill-white px-2 py-1 rounded-md text-xs flex items-center justify-center gap-1 select-none"
-    onClick={onClick}
-  >
-    <div className="flex gap-1 items-center justify-center">
-      <span>{icon}</span>
-      <span>{label}</span>
+}) => {
+  const cn = {
+    base: ['flex', 'items-center', 'justify-center', 'gap-1', 'select-none'],
+    size: {
+      small: ['px-2 py-1 text-xs rounded-md'],
+      medium: ['px-3 py-2 text-md rounded-lg'],
+    },
+    color: {
+      zinc: ['bg-zinc-950 text-white fill-white'],
+      red: ['bg-red-500 text-white fill-white'],
+      blue: ['bg-blue-500 text-white fill-white'],
+      purple: ['bg-purple-500 text-white fill-white'],
+    },
+  }
+
+  const classes =
+    [...cn.base, ...cn.size[size], ...cn.color[color]].join(' ') +
+    ' ' +
+    className
+
+  return (
+    <div className={classes} style={style} onClick={onClick}>
+      <div className="flex gap-1 items-center justify-center">
+        <span>{icon}</span>
+        <span>{label}</span>
+      </div>
+      {isRemovable && <CloseButton onClick={onRemove} />}
     </div>
-    {isRemovable && <CloseButton onClick={onRemove} />}
-  </div>
-)
+  )
+}
 
 interface CloseButtonProps {
   onClick?: () => void
@@ -34,7 +61,7 @@ interface CloseButtonProps {
 const CloseButton: React.FC<CloseButtonProps> = ({ onClick }) => (
   <button
     onClick={onClick}
-    className="hover:bg-white/20 cursor-pointer rounded-sm"
+    className="hover:bg-black/20 cursor-pointer rounded-sm focus-visible:outline-offset-2 focus-visible:outline-purple-400"
   >
     <FiX />
   </button>
