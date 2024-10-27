5fd8574608e6b737c3cceab9d5aa279b771187f0
Todd Moy <todd.moy@gmail.com>
2024-10-26 22:21:18 -0600

    Refine Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,22 +1,52 @@
-import { ReactNode } from 'react'
+import { CSSProperties, ReactNode } from 'react'
 
 interface ButtonProps {
   label?: string
   leadingIcon?: ReactNode
   trailingIcon?: ReactNode
+  className?: string
+  style?: CSSProperties
 }
 
 const Button: React.FC<ButtonProps> = ({
   label = 'Label',
   leadingIcon,
   trailingIcon,
-}) => (
-  <button className="rounded-lg bg-zinc-950 hover:bg-zinc-700 text-white fill-white px-4 py-2 select-none flex justify-center items-center gap-2">
-    {leadingIcon && <ButtonIcon icon={leadingIcon} />}
-    <span>{label}</span>
-    {trailingIcon && <ButtonIcon icon={trailingIcon} />}
-  </button>
-)
+  className,
+  style,
+}) => {
+  const cn = {
+    base: [
+      'rounded-lg',
+      'bg-zinc-950',
+      'hover:bg-zinc-700',
+      'text-white',
+      'fill-white',
+      'px-4',
+      'py-2',
+      'select-none',
+      'flex',
+      'justify-center',
+      'items-center',
+      'gap-2',
+      'focus-visible:outline-offset-2',
+      'focus-visible:outline-purple-400',
+    ],
+  }
+
+  const classes = [...cn.base].join(' ') + ' ' + className
+
+  return (
+    <button className={classes} style={style}>
+      {leadingIcon && <ButtonIcon icon={leadingIcon} />}
+      <span>
+        {label}
+        {className}
+      </span>
+      {trailingIcon && <ButtonIcon icon={trailingIcon} />}
+    </button>
+  )
+}
 
 interface ButtonIconProps {
   icon: ReactNode
