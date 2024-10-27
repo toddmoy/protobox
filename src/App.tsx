2c6c5ad58c91c100b03b8e8311dc2f286a487abf
Todd Moy <todd.moy@gmail.com>
2024-10-26 22:38:30 -0600

    Add parts page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Welcome from './components/Welcome'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Parts from './pages/parts'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" Component={Welcome} />
+        <Route path="/parts" Component={Parts} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/parts.tsx
@@ -0,0 +1,27 @@
+import Tag from '../components/Tag'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import Toggle from '../components/Toggle'
+
+const Parts = () => (
+  <main>
+    <section>
+      <h2>Tag</h2>
+      <Tag />
+    </section>
+    <section>
+      <h2>Button</h2>
+      <Button />
+    </section>
+    <section>
+      <h2>Input</h2>
+      <Input />
+    </section>
+    <section>
+      <h2>Toggle</h2>
+      <Toggle />
+    </section>
+  </main>
+)
+
+export default Parts
