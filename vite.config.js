ba20861a023570e6e5ec7a12a5f9dbd22a135b5f
Todd Moy <todd.moy@gmail.com>
2025-12-03 07:31:10 -0700

    Add localhost run support

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    allowedHosts: [
+      '.lhr.life',
+      '.localhost.run',
+    ],
+  },
 })
