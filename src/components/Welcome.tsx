590ba98bd93adb58f32fa37170e9e63b455db2e8
Todd Moy <todd.moy@gmail.com>
2025-12-27 17:17:22 -0700

    Cleanup

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,13 +1,12 @@
 import { FiBox } from 'react-icons/fi'
 
 const Welcome = () => (
-  <div className="center-content h-screen flex-col gap-4">
-    <div className="flex flex-col justify-center items-center gap-4">
-      <FiBox size={40} />
-      <p className="font-bold">Protobox</p>
+  <div className="center-content h-screen bg-neutral-100">
+    <div className="bg-white border border p-10 center-content flex-col gap-2 rounded-lg">
+      <FiBox size={40} className="text-neutral-800" />
+      <p className="text-neutral-800 font-bold">Protobox</p>
     </div>
   </div>
 )
-
 
 export default Welcome
