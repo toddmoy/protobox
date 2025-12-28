36f62e28aa166c1c85bf0fc20f35131f81c7dbe2
Todd Moy <todd.moy@gmail.com>
2025-12-27 17:07:06 -0700

    Remove cruft

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,53 +1,13 @@
 import { FiBox } from 'react-icons/fi'
-import { motion } from 'framer-motion'
-import { DndContext, useDraggable } from '@dnd-kit/core'
-import { useHotkeys } from 'react-hotkeys-hook'
-import { faker } from '@faker-js/faker'
-import { Badge } from '@/components/ui/badge'
 
-const DraggableText = ({ children }: { children: React.ReactNode }) => {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: 'footer-text',
-  })
-
-  const style = transform
-    ? {
-        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-        cursor: 'grab',
-      }
-    : { cursor: 'grab' }
-
-  return (
-    <p
-      ref={setNodeRef}
-      {...listeners}
-      {...attributes}
-      className="text-xs text-zinc-400"
-      style={style}
-    >
-      {children}
-    </p>
-  )
-}
-
-const Welcome = () => {
-  useHotkeys('ctrl+/', () => {
-    alert('hello')
-  })
-
-  return (
-    <div className="center-content h-screen flex-col gap-4">
-      <motion.div animate={{ y: -20 }} className="flex flex-col justify-center items-center gap-4">
-        <FiBox size={40} />
-
-        <p className="font-bold">Protobox</p>
-      </motion.div>
-      <Badge variant="secondary">Last updated {faker.date.recent().toLocaleDateString()}</Badge>
-      <DndContext>
-        <DraggableText>Created with 🖤 by {faker.person.fullName()}</DraggableText>
-      </DndContext>
+const Welcome = () => (
+  <div className="center-content h-screen flex-col gap-4">
+    <div className="flex flex-col justify-center items-center gap-4">
+      <FiBox size={40} />
+      <p className="font-bold">Protobox</p>
     </div>
-  )
-}
+  </div>
+)
+
 
 export default Welcome
